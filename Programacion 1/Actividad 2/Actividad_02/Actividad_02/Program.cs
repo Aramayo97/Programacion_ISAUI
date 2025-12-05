f4490d85ac7a095ccbd3294d4f93641e9a8735bb
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:02:59 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_02/Actividad_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class A2
+{
+    static void Main()
+    {
+        Console.Write("Cantidad: ");
+        int cant = int.Parse(Console.ReadLine());
+        Console.Write("Precio unitario: ");
+        double precio = double.Parse(Console.ReadLine());
+
+        double total = cant * precio;
+
+        if (total > 10000)
+            total = total * 0.85; // 15% descuento
+
+        Console.WriteLine("Total a pagar: " + total);
+    }
+}
