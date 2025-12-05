ec34d71552dfdd7189f7216b0cc16bd98aa0fc97
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 12:20:51 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 1/Actividad_1/Actividad_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Actividad1
+{
+    static void Main()
+    {
+        int numero, mayor = int.MinValue, menor = int.MaxValue;
+        int sumaPares = 0, cantidadPares = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Ingrese número {i}: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > mayor) mayor = numero;
+            if (numero < menor) menor = numero;
+
+            if (numero % 2 == 0)
+            {
+                sumaPares += numero;
+                cantidadPares++;
+            }
+        }
+
+        double promedio = (cantidadPares > 0) ? (double)sumaPares / cantidadPares : 0;
+
+        Console.WriteLine($"Mayor: {mayor}");
+        Console.WriteLine($"Menor: {menor}");
+        Console.WriteLine($"Promedio de pares: {promedio}");
+    }
+}
