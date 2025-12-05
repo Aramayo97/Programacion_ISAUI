896703bf317d6e468ab907d3f2e3f82ae0ebbe9b
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 16:15:01 -0300

    Add files via upload

---
+++ Programacion 1/Parciales/Ejercicio Parcial/Ejercicio Parcial/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] VecCodigo = new int[8] { 1, 1, 2, 2, 3, 3, 1, 2 };
+            int[] VecVenta = new int[8] { 100, 200, 200, 400, 300, 400, 700, 1000 };
+            int Codigo = 0, Con = 0, Suma = 0, b = 0, VentaMayor = 0;
+            Double Prom = 0;
+            string Mensaje = "";
+            Console.Write("Ingresar el código a buscar: ");
+            Codigo = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < VecCodigo.Length; i++)
+            {
+                if (VecCodigo[i] == Codigo)
+                {
+                    Con++;
+                    Suma = Suma + VecVenta[i];
+                    if (b == 0)
+                    {
+                        VentaMayor = VecVenta[i];
+                        b = 1;
+                    }
+                    if (VecVenta[i] > VentaMayor)
+                        VentaMayor = VecVenta[i];
+
+                }
+            }
+
+            if (Con > 0)
+                Prom = (Double)Suma / Con;
+            Mensaje = "El promedio de ventas del Produco con Código " + Codigo.ToString() + " es: " + Prom.ToString();
+            Console.WriteLine(Mensaje);
+            Mensaje = " La venta mayor del Produco con Código " + Codigo.ToString() + " es: " + VentaMayor.ToString();
+            Console.WriteLine(Mensaje);
+            Console.ReadLine();
+
+        }
+    }
+}
