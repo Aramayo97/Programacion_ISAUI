41c6940ff2c486acc8c53068bee2492bfc398100
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 12:16:42 -0300

    Add files via upload

---
+++ Programacion 1/Prácticos/Practico3/Ejemplo Ciclo While/Program.cs
@@ -0,0 +1,39 @@
+﻿int Con = 0, Suma = 0;
+int Nro = 0, Mayor = 0;
+string Msj = "";
+double Prom = 0;
+
+while (Con < 10)
+{
+    Msj = "Ingresar número " + (Con + 1).ToString();
+    Console.WriteLine(Msj);
+
+    Nro = Convert.ToInt32(Console.ReadLine());
+
+    // Guardar el primer número como mayor
+    if (Con == 0)
+    {
+        Mayor = Nro;
+    }
+
+    // Comparar para actualizar el mayor
+    if (Nro > Mayor)
+    {
+        Mayor = Nro;
+    }
+
+    Suma = Suma + Nro;
+    Con = Con + 1;
+}
+
+Console.WriteLine("------------------------");
+
+Prom = (double)Suma / Con;
+
+Msj = "El promedio es " + Prom.ToString();
+Console.WriteLine(Msj);
+
+Msj = "El mayor número de la serie es " + Mayor.ToString();
+Console.WriteLine(Msj);
+
+Console.ReadLine();
