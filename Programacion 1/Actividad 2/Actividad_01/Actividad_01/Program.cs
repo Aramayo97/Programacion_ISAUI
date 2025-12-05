e629cbfde4dbf8724c17d50f4ca49997bf9fcaab
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:02:11 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_01/Actividad_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class A1
+{
+    static void Main()
+    {
+        int n1, n2, n3;
+        Console.Write("Ingrese número 1: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese número 2: ");
+        n2 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese número 3: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        double prom = (n1 + n2 + n3) / 3.0;
+
+        Console.WriteLine("Promedio: " + prom);
+    }
+}
