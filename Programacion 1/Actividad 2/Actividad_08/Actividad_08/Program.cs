b29c82f41649567ca2e5355a55c4406e34587464
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:07:25 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_08/Actividad_08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class A8
+{
+    static void Main()
+    {
+        int num, mayor1 = 0, menor2 = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+
+            if (i == 1) mayor1 = num;
+            if (i == 6) menor2 = num;
+
+            if (i <= 5 && num > mayor1) mayor1 = num;
+            if (i >= 6 && num < menor2) menor2 = num;
+        }
+
+        Console.WriteLine("Mayor primera mitad: " + mayor1);
+        Console.WriteLine("Menor segunda mitad: " + menor2);
+    }
+}
