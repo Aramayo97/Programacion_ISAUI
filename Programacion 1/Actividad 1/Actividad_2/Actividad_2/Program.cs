63b81ec7fee29e996b6bc77b84febdb81bf6534c
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 12:22:06 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 1/Actividad_2/Actividad_2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class Actividad2
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num * i);
+        }
+    }
+}
