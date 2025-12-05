0401941d094a6e363a7a0d901c5e6091c6104f21
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:08:04 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_09/Actividad_09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class A9
+{
+    static void Main()
+    {
+        int num, suma = 0, cont = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 6 == 0)
+            {
+                suma += num;
+                cont++;
+            }
+        }
+
+        if (cont > 0)
+            Console.WriteLine("Promedio: " + (suma / (double)cont));
+        else
+            Console.WriteLine("No hay números divisibles por 2 y 3.");
+    }
+}
