d31b76bf2bea25ab984244a9b5d88dbf78555e3c
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:06:43 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_07/Actividad_07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class A7
+{
+    static void Main()
+    {
+        int pares = 0, impares = 0, num;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) pares++;
+            else impares++;
+        }
+
+        Console.WriteLine("Pares: " + (pares * 100 / 10.0) + "%");
+        Console.WriteLine("Impares: " + (impares * 100 / 10.0) + "%");
+    }
+}
