686933055b3ea32a5e99341bda85d8c6599da467
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 16:01:28 -0300

    Add files via upload

---
+++ Programacion 1/Vectores/Vectores_1/Vectores_1/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] vec = new int[10];
+int mayor, menor, suma = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Ingrese número: ");
+    vec[i] = int.Parse(Console.ReadLine());
+}
+
+mayor = vec[0];
+menor = vec[0];
+
+for (int i = 0; i < 10; i++)
+{
+    suma += vec[i];
+
+    if (vec[i] > mayor)
+        mayor = vec[i];
+
+    if (vec[i] < menor)
+        menor = vec[i];
+}
+
+double promedio = (double)suma / 10;
+
+Console.WriteLine("Mayor: " + mayor);
+Console.WriteLine("Menor: " + menor);
+Console.WriteLine("Promedio: " + promedio);
