8becf3e22df7660da07af9d349463c54dd22e831
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:05:17 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_05/Actividad_05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class A5
+{
+    static void Main()
+    {
+        int suma = 0, num;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+            suma += num;
+        }
+
+        Console.WriteLine("Promedio: " + (suma / 10.0));
+    }
+}
