3e0e231764a67fb6631f8635258999b6f7df8cd6
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 16:07:06 -0300

    Add files via upload

---
+++ Programacion 1/Vectores/Vectores_5/Vector_5/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] vec = new int[10];
+List<int> pares = new List<int>();
+List<int> impares = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Ingrese número: ");
+    vec[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (vec[i] % 2 == 0)
+        pares.Add(vec[i]);
+    else
+        impares.Add(vec[i]);
+}
+
+Console.WriteLine("Pares:");
+foreach (int n in pares)
+    Console.Write(n + " ");
+
+Console.WriteLine("\nImpares:");
+foreach (int n in impares)
+    Console.Write(n + " ");
---
+++ Programacion 1/Vectores/Vectores_6/Vector_6/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] A = new int[5];
+int[] B = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("A[" + i + "]: ");
+    A[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    B[i] = A[4 - i];
+}
+
+Console.WriteLine("Vector invertido:");
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(B[i] + " ");
+}
