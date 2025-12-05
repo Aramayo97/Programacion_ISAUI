c7ca4d283fa20812305bddaa7a16f3ca6f7cad33
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:04:27 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_04/Actividad_04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class A4
+{
+    static void Main()
+    {
+        Console.Write("Precio unitario: ");
+        double precio = double.Parse(Console.ReadLine());
+
+        Console.Write("Cantidad: ");
+        int cant = int.Parse(Console.ReadLine());
+
+        Console.Write("Código de pago (1=Efectivo, 2=Crédito): ");
+        int cod = int.Parse(Console.ReadLine());
+
+        double subtotal = precio * cant;
+        double ajuste = 0;
+        double total = subtotal;
+
+        if (cod == 1)
+        {
+            if (subtotal > 10000)
+            {
+                ajuste = subtotal * 0.10;
+                total = subtotal - ajuste;
+            }
+        }
+        else if (cod == 2)
+        {
+            ajuste = subtotal * 0.30;
+            total = subtotal + ajuste;
+        }
+
+        Console.WriteLine("Subtotal: " + subtotal);
+        Console.WriteLine("Ajuste (+/-): " + ajuste);
+        Console.WriteLine("Total a pagar: " + total);
+    }
+}
