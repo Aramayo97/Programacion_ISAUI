bc7ddb286728a1357647480a6b0bff772c7abad9
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 12:21:31 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 1/Acividad_6/Acividad_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Actividad6
+{
+    static void Main()
+    {
+        int numero;
+        int mayor = int.MinValue;
+        bool existe = false;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Ingrese número {i}: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 6 == 0)
+            {
+                if (numero > mayor) mayor = numero;
+                existe = true;
+            }
+        }
+
+        if (existe)
+            Console.WriteLine("El mayor divisible por 2 y 3 es: " + mayor);
+        else
+            Console.WriteLine("No se ingresaron números divisibles por 2 y 3.");
+    }
+}
