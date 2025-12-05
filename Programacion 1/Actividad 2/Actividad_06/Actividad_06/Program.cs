70b6a4c39a4eddb0f8ff41b993f907429d39fb36
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:06:00 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_06/Actividad_06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class A6
+{
+    static void Main()
+    {
+        int num, mayor = 0, menor = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+
+            if (i == 1)
+            {
+                mayor = menor = num;
+            }
+
+            if (num > mayor) mayor = num;
+            if (num < menor) menor = num;
+        }
+
+        Console.WriteLine("Mayor: " + mayor + " - Menor: " + menor);
+    }
+}
