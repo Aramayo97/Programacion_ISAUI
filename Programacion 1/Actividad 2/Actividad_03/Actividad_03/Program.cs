7201eddcb27d1eb189714689b8d6fd65be85c77b
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:03:45 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_03/Actividad_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class A3
+{
+    static void Main()
+    {
+        Console.Write("Inglés: ");
+        int ing = int.Parse(Console.ReadLine());
+        Console.Write("Francés: ");
+        int fra = int.Parse(Console.ReadLine());
+        Console.Write("Portugués: ");
+        int por = int.Parse(Console.ReadLine());
+
+        int total = ing + fra + por;
+
+        Console.WriteLine("Inglés: " + (ing * 100.0 / total) + "%");
+        Console.WriteLine("Francés: " + (fra * 100.0 / total) + "%");
+        Console.WriteLine("Portugués: " + (por * 100.0 / total) + "%");
+    }
+}
