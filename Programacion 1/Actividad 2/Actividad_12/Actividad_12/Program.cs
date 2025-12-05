d7f3c2091b38b019d30cd657725e2eb051a0f099
Aramayo Yamila Iara <yamilaaramayo59@gmail.com>
2025-12-05 15:19:43 -0300

    Add files via upload

---
+++ Programacion 1/Actividad 2/Actividad_12/Actividad_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class A12
+{
+    static void Main()
+    {
+        int dia, diaMayor = 0;
+        double min, max, mayorAmp = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Día (número): ");
+            dia = int.Parse(Console.ReadLine());
+
+            Console.Write("Temp mínima: ");
+            min = double.Parse(Console.ReadLine());
+            Console.Write("Temp máxima: ");
+            max = double.Parse(Console.ReadLine());
+
+            double amp = max - min;
+
+            if (i == 1 || amp > mayorAmp)
+            {
+                mayorAmp = amp;
+                diaMayor = dia;
+            }
+        }
+
+        Console.WriteLine("El día con mayor amplitud fue el día: " + diaMayor);
+    }
+}
